f4d9d3e5959000b40dd18146a5128cbb9c71f7de
Raghav Krishna <raghav@Raghavs-MacBook-Air.local>
2025-05-18 00:34:59 +0530

    .

--- slides.js
+++ slides.js
@@ -1,28 +1,26 @@
 export const slides = [
   {
    
-    url: "https://example.com/alpha",
+   //direct to socialmoodboard
     image: "/1.mp4",
   },
   {
   
-    url: "https://example.com/beta",
+ 
     image: "/2.mp4",
   },
   
   {
-  
-    url: "https://example.com/delta",
+
     image: "/4.mp4",
   },
   {
 
-    url: "https://example.com/epsilon",
+
     image: "/5.mp4",
   },
   {
-   
-    url: "https://example.com/zeta",
+
     image: "/6.mp4",
   },
 
